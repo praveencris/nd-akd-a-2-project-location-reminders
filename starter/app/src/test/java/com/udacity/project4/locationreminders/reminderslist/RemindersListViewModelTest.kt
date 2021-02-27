0354d3b1f9ec524e3cae690bb52529bd49574d32
Praveen <pkchmits@gmail.com>
2021-02-27 11:15:58 +0530

    Successfully added testcase for ReminderListViewModel and liveData with biolerplate code

--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,63 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.app.usage.UsageEvents
+import android.os.Build
+import android.util.EventLog
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.Event
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.utils.SingleLiveEvent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.apache.tools.ant.taskdefs.Tstamp
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
+@Config(sdk = [Build.VERSION_CODES.P])
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
+
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+
+    @Test
+    fun loadReminders_addRemindersToList() {
+        //Given a fresh viewModel
+        val reminderDataSource = FakeDataSource()
+        val remindersListViewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(),
+            reminderDataSource
+        )
+        //Create observer - no need for it to do anything
+        val observer= Observer<List<ReminderDataItem>> {}
+        try {
+            // Observe live data forever
+            remindersListViewModel.remindersList.observeForever(observer)
+
+            //When loading reminders
+            remindersListViewModel.loadReminders()
+
+
+            //Then reminderList event triggered
+            val value:List<ReminderDataItem>? = remindersListViewModel.remindersList.value
+            assertThat(value.orEmpty(), empty())
+        }finally {
+            //Whatever happens, don't forget to remove observer!
+            remindersListViewModel.remindersList.removeObserver(observer)
+        }
+    }
 
 }
